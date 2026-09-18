857f95adc0a3d4389b305667aba3e0a2b52b773d
smenychillpoint <smenychillpoint@gmail.com>
2026-09-18 18:58:14 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+// Jednoduchý service worker: vždy skúsi najprv sieť (aktuálne dáta a verzia), keď nie je internet, použije uloženú kópiu stránky.
+const CACHE = 'cp-smeny-v1';
+const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).catch(() => {}));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  if (new URL(req.url).origin !== location.origin) return;   // Firebase a ostatné externé veci idú priamo
+  e.respondWith(
+    fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(req, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match(req).then(r => r || caches.match('./index.html')))
+  );
+});
